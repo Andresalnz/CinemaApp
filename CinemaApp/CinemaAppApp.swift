81a5e0b34f51241177349b9d2d459c40b5e3bd20
Andresalnz <andresaleunz@gmail.com>
2024-01-17 13:36:17 +0100

    Initial Commit

---
+++ CinemaApp/CinemaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaAppApp.swift
+//  CinemaApp
+//
+//  Created by Andres Aleu on 17/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CinemaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
